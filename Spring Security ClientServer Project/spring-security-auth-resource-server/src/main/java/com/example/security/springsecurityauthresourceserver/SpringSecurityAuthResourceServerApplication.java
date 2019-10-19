945a9f82df036c5f3a5717fe5167f6de926f1fc7
Hamsalekha Venkatesh <hamsalekhavenkatesh@Hams-MacBook-Air.local>
2019-10-19 13:30:35 -0700

    Sample oauth resource server using spring boot and oauth 2.0

---
+++ Spring Security ClientServer Project/spring-security-auth-resource-server/src/main/java/com/example/security/springsecurityauthresourceserver/SpringSecurityAuthResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.security.springsecurityauthresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthResourceServerApplication.class, args);
+	}
+
+}
---
+++ Spring Security ClientServer Project/spring-security-auth-resource-server/src/test/java/com/example/security/springsecurityauthresourceserver/SpringSecurityAuthResourceServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.security.springsecurityauthresourceserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityAuthResourceServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
